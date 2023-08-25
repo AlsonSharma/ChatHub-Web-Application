1d3fa496e6830769049b6464f905110c2c9fd5b4
Student <sharmaalson@gmail.com>
2023-08-25 17:47:58 -0400

    Initial commit

---
+++ nodeServer/index.js
@@ -0,0 +1,30 @@
+// It is a nodeServer to handle socket io connection
+const io = require('socket.io')(8090, {
+    cors: {
+      origin: 'http://127.0.0.1:5500',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type']
+    }
+  });
+  
+  const users = {};
+  
+  io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+      // console.log("New User:", name);
+      users[socket.id] = name;
+      socket.broadcast.emit('user-joined', name);
+    });
+  
+    socket.on('send', message => {
+      socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+    socket.on('disconnect', message => {
+      socket.broadcast.emit('left', users[socket.id]);
+      delete users[socket.id];
+    });
+  
+    // Send the user's name back to the client for confirmation
+    socket.emit('name-confirmation', users[socket.id]);
+  });
+  
